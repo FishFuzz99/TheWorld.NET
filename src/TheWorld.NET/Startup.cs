b6ceb7ce4839707bba5a0bce91bad07971956dd0
FishFuzz99 <fishfuzz99@gmail.com>
2016-01-18 00:31:41 -0700

    adding current project state. Old repo was corrupted. Just finished MVC 6 and jQuery

---
+++ src/TheWorld.NET/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.Configuration;
+using Microsoft.Framework.DependencyInjection;
+using TheWorld.Services;
+using Microsoft.Dnx.Runtime;
+
+namespace TheWorld.NET
+{
+    public class Startup
+    {
+        public static IConfigurationRoot Configuration;
+
+        public Startup(IApplicationEnvironment appEnv)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(appEnv.ApplicationBasePath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+#if DEBUG
+            services.AddScoped<IMailService, DebugMailService>();
+#else
+            services.AddScoped<IMailService, RealMailService>();
+#endif
+
+
+        }
+
+        public void Configure(IApplicationBuilder app)
+        { // order matters
+            app.UseStaticFiles();
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "App", Action = "index" }
+                    );
+            });
+        }
+
+         //public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
---
+++ src/TheWorld.NET/wwwroot/js/site.js
@@ -0,0 +1,41 @@
+﻿// site.js
+(function () {
+    
+    
+    var $sidebarAndWrapper = $("#sidebar,#wrapper"); // wrapped set this AND that //$naming shows its a jQuery variable
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Hide Sidebar");
+        }
+        else {
+            $(this).text("Show Sidebar");
+        }
+    })
+    
+
+
+
+
+
+    //var ele = $("#username");
+    //ele.text("Abraham Lincoln")
+
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.style = "background-color: #888;";
+    //});
+    //main.on("mouseleave", function () {
+    //    main.style = "";
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function()     
+    //{
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+})();
+
+
